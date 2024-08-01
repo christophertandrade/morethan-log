daf5198bdcde42abff0510febec5ee6959796300
christophertandrade <christophertandrade@gmail.com>
2024-08-01 13:27:45 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Christopher Andrade",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    role: "Student",
+    bio: "Network Engineering and Security Student",
+    email: "christophertandrade@gmail.com",
     linkedin: "morethanmin",
     github: "morethanmin",
     instagram: "",
