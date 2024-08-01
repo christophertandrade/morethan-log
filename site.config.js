b3670c6cdd54fca7fd6c9aa050a774c6832df0df
christophertandrade <christophertandrade@gmail.com>
2024-08-01 14:41:37 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Christopher Andrade",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/notion-avatar-1722548424402.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Student",
     bio: "Network Engineering and Security Student",
     email: "christophertandrade@gmail.com",
